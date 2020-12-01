f8875a4404de3f266bac0ca6ed5ef0e8865da131
Vitor Dal Pra <vitordalpra@gmail.com>
2020-12-01 14:35:26 -0300

    carousel working

---
+++ app/javascript/components/carousel.js
@@ -0,0 +1,10 @@
+const carousel = () => {
+  const slider = tns({
+      container: '.teams-carrousel',
+      items: 4,
+      slideBy: 'page',
+      autoplay: true
+  });
+};
+
+export { carousel };
