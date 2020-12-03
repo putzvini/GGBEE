c8d57e6d8d557dfbc5ae3ee7904844e9288ad4df
Vitor Dal Pra <vitordalpra@gmail.com>
2020-12-03 12:53:57 -0300

    logos added, colors fixed, alignments

--- app/javascript/components/carousel.js
+++ app/javascript/components/carousel.js
@@ -1,7 +1,7 @@
 const carousel = () => {
   const slider = tns({
       container: '.teams-carrousel',
-      items: 6,
+      items: 7,
       slideBy: 'page',
       nav: false,
       rewind: true
