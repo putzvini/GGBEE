ffbc4c72f68f40be6b110c803de9c039bfcf1faf
Vitor Dal Pra <vitordalpra@gmail.com>
2020-12-03 12:16:54 -0300

    added cards

--- app/javascript/components/carousel.js
+++ app/javascript/components/carousel.js
@@ -1,7 +1,7 @@
 const carousel = () => {
   const slider = tns({
       container: '.teams-carrousel',
-      items: 5,
+      items: 6,
       slideBy: 'page',
       nav: false,
       rewind: true
