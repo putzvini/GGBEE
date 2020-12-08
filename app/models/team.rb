e23ea4c0085132ed1290ebe55adc6dadb3fa1d5a
vinicio oliveira <um.vinicio@gmail.com>
2020-12-07 22:54:06 -0300

    teams cards

--- app/models/team.rb
+++ app/models/team.rb
@@ -3,6 +3,36 @@ class Team < ApplicationRecord
   validates :team_name, :team_long_name, :team_tag, presence: true
 end
 
+def top_5_teams
+  teams_array = []
+  Team.all.each do |team|
+    arg = Match.select(:match_view).where(red_team_id: team.id).or(Match.select(:match_view).where(blue_team_id: team.id))
+    views = []
+    arg.each do |match|
+      views << match[:match_view]
+    end
+    matches = views.count
+    avg_views = (views.sum / matches).round
+    args2 = Match.select(:match_like).where(red_team_id: team.id).or(Match.select(:match_like).where(blue_team_id: team.id))
+    likes = []
+    args2.each do |match|
+      likes << match[:match_like]
+    end
+    avg_likes = (likes.sum / matches)
+    hash = {}
+    hash = {
+      team_id: team.id,
+      team_tag: team.team_tag,
+      team: team.team_name,
+      matches: matches,
+      avg_views: avg_views,
+      avg_likes: avg_likes
+    }
+    teams_array << hash
+  end
+  response = teams_array.sort_by { |hsh| hsh[:avg_views]}.last(5).reverse
+end
+
 def top_avg_chart(data)
   response = {}
   Team.all.each do |team|
@@ -39,7 +69,7 @@ def banner_infos_team(id)
   response
 end
 
-def last_5(id)
+def last_5_matches(id)
   response = []
   var = Match.where(red_team_id: id).or(Match.where(blue_team_id: id)).sort_by{|match| match.match_date}.last(5)
   var.each_with_index do |match, i|
@@ -51,7 +81,7 @@ def last_5(id)
   response
 end
 
-def top_5(id)
+def top_5_matches(id)
   response = []
   var = Match.where(red_team_id: id).or(Match.where(blue_team_id: id)).sort_by{|match| match.match_view}.last(5)
 
